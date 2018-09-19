a7dcf529303cfa34749f9f2b21c955d792b02735
michaelortega <michaelortega93@gmail.com>
2018-09-19 03:17:16 -0700

    Created User app, Created User Model for Database, Created User Sign Up form, Created superuser for admin panel

--- Scripter/Scripter/settings.py
+++ Scripter/Scripter/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'users.apps.UsersConfig',
     'scripter_app.apps.ScripterAppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
